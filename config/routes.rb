88efa0f783bce794cf0df65cd88abbd938231fdd
Laura Felce <crepu83@yahoo.it>
2019-06-27 11:32:40 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :authors, only: [:show] do
 
   resources :posts, only: [:index, :show]
-end
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
