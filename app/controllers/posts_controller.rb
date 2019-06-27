78384ca17e99097e2f1989a807c510bd11528914
Laura Felce <crepu83@yahoo.it>
2019-06-27 11:27:21 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,5 @@
 class PostsController < ApplicationController
 
-  def posts_index
-    @author = Author.find(params[:id])
-    @posts = @author.posts
-    render template: 'posts/index'
-  end
-
-  def post
-    @author = Author.find(params[:id])
-    @post = Post.find(params[:post_id])
-    render template: 'posts/show'
-  end  
-
   def index
     @posts = Post.all
   end
